957af79fc7d500e8570295e635c4a048c9944a94
Yixiyihan <972341480@qq.com>
2019-12-20 03:17:59 +0800

    后台

--- Application/Ladmin/Controller/IndexController.class.php
+++ Application/Ladmin/Controller/IndexController.class.php
@@ -22,22 +22,22 @@ class IndexController extends BaseController {
                 }
             }
         }else{
-            $out = Array(
-                [0] => "/www/wwwroot/NewLunXun ",
-                [1] => "From https://github.com/ninging123/NewLunxun ",
-                [2] => "03bed86..90fbcbc master -> origin/master ",
-                [3] => "error: Your local changes to the following files would be overwritten by merge: ",
-                [4] => "Application/Common/Conf/config.php ",
-                [5] => "Application/Ladmin/Common/function.php ",
-                [6] => "Application/Ladmin/Controller/BaseController.class.php ",
-                [7] => "Application/Ladmin/Controller/LoginController.class.php ",
-                [8] => "Application/Ladmin/Model/LongModel.class.php ",
-                [9] => "Application/Ladmin/View/Login/index.html ",
-                [10] => "Please, commit your changes or stash them before you can merge. ",
-                [11] => "Aborting ",
-                [12] => "Updating 03bed86..90fbcbc"
+            $out = array(
+                '0' => "/www/wwwroot/NewLunXun ",
+                '1' => "From https://github.com/ninging123/NewLunxun ",
+                '2' => "03bed86..90fbcbc master -> origin/master ",
+                '3' => "error: Your local changes to the following files would be overwritten by merge: ",
+                '4' => "Application/Common/Conf/config.php ",
+                '5' => "Application/Ladmin/Common/function.php ",
+                '6' => "Application/Ladmin/Controller/BaseController.class.php ",
+                '7' => "Application/Ladmin/Controller/LoginController.class.php ",
+                '8' => "Application/Ladmin/Model/LongModel.class.php ",
+                '9' => "Application/Ladmin/View/Login/index.html ",
+                '10' => "Please, commit your changes or stash them before you can merge. ",
+                '11' => "Aborting ",
+                '12' => "Updating 03bed86..90fbcbc"
             );
-            foreach ($out as $k=>$v){
+            foreach ($out as $v){
                 if (strstr($v,'error')){
                     $shell_pro = "git stash && git pull && git stash pop";
                     exec($shell_pro,$our_pro);
